4f19319a54b141ad388d89ab6f7111687ecf3567
Daniel Woodrich <d.woodrich1@gmail.com>
2019-04-10 00:52:58 -0700

    save full experiment. Might have miscalculated AUC on NEW data

--- PlotPerformance.R
+++ PlotPerformance.R
@@ -8,22 +8,25 @@ dataALL<-data[which(data$Type=="ALL"),]
 
 
 boxplot(dataGT$AUC ~ dataGT$Test, data = dataGT, lwd = 2, ylab = 'AUC',ylim = c(min(dataNEW$AUC)-0.01,1),xlab="Experiment",main="")
-stripchart(dataNEW$AUC ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
-            add = TRUE, pch = 18, col = 'red',lwd=4)
 stripchart(dataALL$AUC ~ dataALL$Test, vertical = TRUE, data = dataALL, 
            add = TRUE, pch = 12, col = 'blue',lwd=3)
+stripchart(dataNEW$AUC ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
+            add = TRUE, pch = 18, col = 'red',lwd=4)
+
 
 
 boxplot(dataGT$TPR ~ dataGT$Test, data = dataGT, lwd = 2, ylab = 'TPR',ylim = c(min(dataNEW$TPR)-0.01,1),xlab="Experiment")
-stripchart(dataNEW$TPR ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
-            add = TRUE, pch = 18, col = 'red',lwd=4)
 stripchart(dataALL$TPR ~ dataALL$Test, vertical = TRUE, data = dataALL, 
            add = TRUE, pch = 12, col = 'blue',lwd=3)
+stripchart(dataNEW$TPR ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
+            add = TRUE, pch = 18, col = 'red',lwd=4)
+
 
 
 boxplot(dataGT$FPR ~ dataGT$Test, data = dataGT, lwd = 2, ylab = 'FPR',ylim = c(min(dataNEW$FPR)-0.01,1),xlab="Experiment")
-stripchart(dataNEW$FPR ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
-            add = TRUE, pch = 18, col = 'red',lwd=4)
 stripchart(dataALL$FPR ~ dataALL$Test, vertical = TRUE, data = dataALL, 
            add = TRUE, pch = 12, col = 'blue',lwd=3)
+stripchart(dataNEW$FPR ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
+            add = TRUE, pch = 18, col = 'red',lwd=4)
+
 
