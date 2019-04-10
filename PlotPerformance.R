bacb37d07f696e4e9c9e3430477935728c22d5aa
Daniel Woodrich <d.woodrich1@gmail.com>
2019-04-09 23:04:56 -0700

    save progress on experiment

--- PlotPerformance.R
+++ PlotPerformance.R
@@ -4,17 +4,26 @@ data<-read.csv("E:/RavenBLEDscripts/Data/Paper/AddDataSummaryGS.csv")
 
 dataGT<-data[which(data$Type=="GT"),]
 dataNEW<-data[which(data$Type=="NEW"),]
+dataALL<-data[which(data$Type=="ALL"),]
 
 
 boxplot(dataGT$AUC ~ dataGT$Test, data = dataGT, lwd = 2, ylab = 'AUC',ylim = c(min(dataNEW$AUC)-0.01,1),xlab="Experiment",main="")
 stripchart(dataNEW$AUC ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
             add = TRUE, pch = 18, col = 'red',lwd=4)
+stripchart(dataALL$AUC ~ dataALL$Test, vertical = TRUE, data = dataALL, 
+           add = TRUE, pch = 12, col = 'blue',lwd=3)
+
 
 boxplot(dataGT$TPR ~ dataGT$Test, data = dataGT, lwd = 2, ylab = 'TPR',ylim = c(min(dataNEW$TPR)-0.01,1),xlab="Experiment")
 stripchart(dataNEW$TPR ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
             add = TRUE, pch = 18, col = 'red',lwd=4)
+stripchart(dataALL$TPR ~ dataALL$Test, vertical = TRUE, data = dataALL, 
+           add = TRUE, pch = 12, col = 'blue',lwd=3)
+
 
 boxplot(dataGT$FPR ~ dataGT$Test, data = dataGT, lwd = 2, ylab = 'FPR',ylim = c(min(dataNEW$FPR)-0.01,1),xlab="Experiment")
 stripchart(dataNEW$FPR ~ dataNEW$Test, vertical = TRUE, data = dataNEW, 
             add = TRUE, pch = 18, col = 'red',lwd=4)
+stripchart(dataALL$FPR ~ dataALL$Test, vertical = TRUE, data = dataALL, 
+           add = TRUE, pch = 12, col = 'blue',lwd=3)
 
